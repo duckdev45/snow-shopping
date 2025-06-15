6e8212768f82e1646c7a38e2978e4ec44f188bfa
duck <yth@hismax.onmicrosoft.com>
2025-06-16 01:28:59 +0800

    chore: cicd setting

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: import('next').NextConfig = {
+    output: 'export',
 };
 
 export default nextConfig;
